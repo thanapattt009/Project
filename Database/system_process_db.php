797fd871b446c88a65917ac391796eefad12113f
thanapattt009 <thanapattt009@hotmail.com>
2020-02-27 17:12:09 +0700

    ssss

--- Database/system_process_db.php
+++ Database/system_process_db.php
@@ -6,19 +6,42 @@ include 'config.php';
 
 $_POST['Function_Name']();
 
-function addUser(){
-    $Data = json_decode($_POST['_Data']);
-    $IdUser = $Data->id_user;
-    $UsernameUser = $Data->username_user;
-    $PasswordUser = $Data->password_user;
-    $NameUser = $Data->name_user;
-    $EmailUser = $Data->email_user;
-    $TelUser = $Data->tel_user;
-    $conn = getDB();
-    $sql_query = "INSERT INTO user (id_user,username_user,password_user,name_user,email_user,tel_user) VALUES ('$IdUser','$UsernameUser','$PasswordUser','$NameUser','$EmailUser','$TelUser')";
 
+function getAllDataAdmin(){
+    $Data = json_decode($_POST['_Data']);
+    $conn = getDB();    
+    $sql_query = "SELECT * from admin_db";
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $conn->query($sql_query);
+    $rst = $conn->query($sql_query);
+    $Response_Data = $rst->fetchAll(PDO::FETCH_OBJ);      
+    $Response_Data = json_encode($Response_Data);
+    echo $Response_Data;         
+}
+
+function getAllDataUser(){
+    $Data = json_decode($_POST['_Data']);
+    $conn = getDB();    
+    $sql_query = "SELECT * from user";
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn->query($sql_query);
+    $rst = $conn->query($sql_query);
+    $Response_Data = $rst->fetchAll(PDO::FETCH_OBJ);      
+    $Response_Data = json_encode($Response_Data);
+    echo $Response_Data;         
+}
+
+
+function getAllDataUserHead(){
+    $Data = json_decode($_POST['_Data']);
+    $conn = getDB();    
+    $sql_query = "SELECT * from userhead";
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn->query($sql_query);
+    $rst = $conn->query($sql_query);
+    $Response_Data = $rst->fetchAll(PDO::FETCH_OBJ);      
+    $Response_Data = json_encode($Response_Data);
+    echo $Response_Data;         
 }
 
 function getByIdAdmin(){
@@ -50,7 +73,19 @@ function updateAdmin(){
 }
 
 
-
+function addUser(){
+    $Data = json_decode($_POST['_Data']);
+    $IdUser = $Data->id_user;
+    $UsernameUser = $Data->username_user;
+    $PasswordUser = $Data->password_user;
+    $NameUser = $Data->name_user;
+    $EmailUser = $Data->email_user;
+    $TelUser = $Data->tel_user;
+    $conn = getDB();
+    $sql_query = "INSERT INTO user (id_user,username_user,password_user,name_user,email_user,tel_user) VALUES ('$IdUser','$UsernameUser','$PasswordUser','$NameUser','$EmailUser','$TelUser')";
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn->query($sql_query);
+}
 
 function getByIdUser(){
     $Data = json_decode($_POST['_Data']);
